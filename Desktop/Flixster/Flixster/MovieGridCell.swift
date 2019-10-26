40f4b2a4a83e02837a361f40a2055e9ac584e653
Daway101 <dawa_247tsering@yahoo.com>
2019-10-26 01:41:55 -0400

    Extend Flicks app to allow users to get more information about a particular movie in a movie details screen. Then, add a tab bar to allow users to view movies in a grid view (Collection View) from different endpoints of the MovieDB API.

---
+++ Desktop/Flixster/Flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by DawAyush on 10/26/19.
+//  Copyright © 2019 DawAyush. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
